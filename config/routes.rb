ba8bde966ced5bf94e6c4b869a9560963dd7189b
lea sordoillet <leasordoillet@gmail.com>
2020-05-19 14:58:12 +0200

    added the possibility to create a task

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
-  get "tasks/:id", to: "tasks#show", as: :task
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create'
 end
